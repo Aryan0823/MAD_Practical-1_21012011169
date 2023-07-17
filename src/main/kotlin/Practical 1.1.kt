ab825f8488437f11fde357ed3262e1befa042b3d
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2023-07-17 10:43:17 +0530

    Helloworld

--- src/main/kotlin/Practical 1.1.kt
+++ src/main/kotlin/Practical 1.1.kt
@@ -1,4 +1,14 @@
 fun main(){
 //    println("Hello Word")
+    var a:Int=1
+    var b:Float=1.3f
+    var c:String="aryan"
+    var d:Double =12.12
+    var e:Long=1000000000
+    println("$a")
+    println("$b")
+    println("$c")
+    println("$d")
+    println("$e")
 
 }
