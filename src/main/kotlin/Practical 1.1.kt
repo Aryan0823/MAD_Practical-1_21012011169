7a7b3ddb8b05c11a64ea15939bdb5ed2d9ae153f
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2023-07-18 09:31:54 +0530

    Practical 1.1

--- src/main/kotlin/Practical 1.1.kt
+++ src/main/kotlin/Practical 1.1.kt
@@ -5,10 +5,16 @@ fun main(){
     var c:String="aryan"
     var d:Double =12.12
     var e:Long=1000000000
-    println("$a")
-    println("$b")
-    println("$c")
-    println("$d")
-    println("$e")
-
+    var f:Char='A'
+    var g:Boolean = true
+    var h:Byte = 101
+    println("Aryan_21012011169")
+    println("Integer : "+"$a")
+    println("Float   : "+"$b")
+    println("String  : "+"$c")
+    println("Double  : "+"$d")
+    println("Long    : "+"$e")
+    println("Char    : "+"$f")
+    println("Boolean : "+"$g")
+    println("Byte    : "+"$h")
 }
