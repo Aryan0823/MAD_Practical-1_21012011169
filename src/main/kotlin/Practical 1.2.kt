f5ca32bcf375d625fd87c957a5e910ef946335e7
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2023-07-18 09:50:21 +0530

    Practical 1.2

---
+++ src/main/kotlin/Practical 1.2.kt
@@ -0,0 +1,14 @@
+fun main(){
+//    println("Hello Word")
+    println("Aryan_21012011169")
+    var int =30
+    println("Integer value = "+int)
+    val double = int.toDouble()
+    println("Double value from integer = "+double)
+    val string = 45
+    println("String value = "+string)
+    val int1 = string.toInt()
+    println("Integer value from string = "+int1)
+    val int2 = string.toDouble()
+    println("Double value from string = "+int2)
+}
