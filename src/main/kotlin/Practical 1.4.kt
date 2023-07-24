4df7befa577eef692f6615654a4005f087c818dc
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2023-07-24 09:03:51 +0530

    18/7/23 Practical 1.3

---
+++ src/main/kotlin/Practical 1.4.kt
@@ -0,0 +1,10 @@
+fun main(){
+//    println("Hello Word")
+    println("Aryan_21012011169")
+    println("Enter a number: ");
+    val n1 = Integer.valueOf(readLine())
+    if(n1 % 2 == 0)
+        println("$n1 is an Even Number")
+    else
+        println("$n1 is an Odd Number")
+}
