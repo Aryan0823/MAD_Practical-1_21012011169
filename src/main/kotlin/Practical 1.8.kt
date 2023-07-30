c8ee3881807b761ace787aba595db01a31c56acb
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2023-07-30 23:26:30 +0530

    24/7

--- src/main/kotlin/Practical 1.8.kt
+++ src/main/kotlin/Practical 1.8.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Aryan_21012011169")
+    print("Enter Number: ")
+    var number = readLine()!!.toInt()
+    val factorial = fact(number)
+    println("Factorial of $number = $factorial")
+}
+
+tailrec fun fact(n: Int, temp: Int = 1): Int {
+    return if (n == 1){
+        temp
+    } else {
+        fact(n-1, temp*n)
+    }
 }
